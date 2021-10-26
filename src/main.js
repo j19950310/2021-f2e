c333cab3bc3a09c5c196fb7ad2e59678fa33c4ba
Evan <evan@blockstudio.tw>
2021-10-26 22:25:52 +0800

    [data] 調整init架構

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+import router from '@/router/index'
+import store from '@/store/index'
+import '@/style/_main.scss'
+import 'virtual:svg-icons-register'
+import directives from '@/plugins/directives/index'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(directives)
+app.use(router)
+app.use(store)
+
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueComponents from 'unplugin-vue-components/vite'
+import eslintPlugin from 'vite-plugin-eslint'
+import viteSvgIcons from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+    return defineConfig({
+        resolve: {
+            alias: [
+                { find: '@', replacement: '/src' },
+            ],
+        },
+        server: {
+            port: process.env.VITE_APP_PORT,
+            proxy: {
+                '^/assets': {
+                    target: process.env.VITE_APP_URL + 'src/',
+                },
+            },
+        },
+        plugins: [
+            vue(),
+            vueComponents(),
+            eslintPlugin(),
+            viteSvgIcons({
+                iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+                symbolId: '[name]',
+            }),
+        ],
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "./src/style/mixins/mixin";',
+                },
+            },
+        },
+    })
+}
