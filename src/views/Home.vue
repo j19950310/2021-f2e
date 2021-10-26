ebbada4bced6547809594a000bc5608528fb952b
Jay <jaywu.fe@gmail.com>
2021-10-26 20:56:27 +0800

    add vuex, vue-router

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+<div>view - home</div>
+</template>
+
+<style>
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    alias: {
+        "@": resolve(__dirname, 'src'),
+    },
+    plugins: [vue()]
 })
