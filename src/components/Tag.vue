4672797c1ad11fe9882cd7a26655e0d49e101f33
Evan <evan@blockstudio.tw>
2021-11-12 00:11:40 +0800

    [feat] tag

---
+++ src/components/Tag.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        default: null,
+    },
+})
+</script>
+
+<template>
+    <div class="tag">
+        {{ name }}
+    </div>
+</template>
+
+<style lang="scss">
+.tag {
+    @include typo-min;
+
+    display: flex;
+    align-items: center;
+
+    &::before {
+        @include size(8px);
+
+        content: '';
+        display: block;
+        margin-right: $padding / 2;
+        background-color: color('Primary');
+        border-radius: 50%;
+    }
+}
+</style>
