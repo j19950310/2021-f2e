9fd5578511255dbc34f063128d7fa761c47646b2
Evan <evan@blockstudio.tw>
2021-11-13 21:10:05 +0800

    [feat] copy

---
+++ src/components/Copy.vue
@@ -0,0 +1,53 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        default: null,
+    },
+})
+const emit = defineEmits(['on-copy'])
+const isiOS = () => navigator.userAgent.match(/ipad|iphone/i)
+
+const copy = () => {
+    const textarea = document.createElement('textarea')
+    document.body.appendChild(textarea)
+    textarea.value = props.text || window.location.href
+
+    if (isiOS()) {
+        const range = document.createRange()
+        const selection = window.getSelection()
+        range.selectNodeContents(textarea)
+        selection.removeAllRanges()
+        selection.addRange(range)
+        textarea.setSelectionRange(0, 999999)
+    } else textarea.select()
+
+    document.execCommand('copy')
+    document.body.removeChild(textarea)
+    emit('on-copy')
+}
+</script>
+
+<template>
+    <div
+        class="copy"
+        @click="copy"
+    >
+        <Icon name="copy" />
+    </div>
+</template>
+
+<style lang="scss">
+.copy {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    padding: $padding;
+    justify-self: center;
+    @media (hover: hover) and (pointer: fine) {
+        &:hover {
+            color: color('Primary');
+        }
+    }
+}
+</style>
