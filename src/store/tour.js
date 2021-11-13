2026551a9fdaf8f6288c64f0297dffdeb678101b
Jay <jaywu.fe@gmail.com>
2021-11-13 18:37:42 +0800

    Store 查詢彙整

--- src/store/tour.js
+++ src/store/tour.js
@@ -35,6 +35,20 @@ const QUERY_HOTEL_BY_CITY = 'QUERY_HOTEL_BY_CITY'
 const QUERY_ACTIVITY = 'QUERY_ACTIVITY'
 const QUERY_ACTIVITY_BY_CITY = 'QUERY_ACTIVITY_BY_CITY'
 
+const FILTER_BASIC_TRUE = '1 == 1'
+
+const BASIC_FILTER = { // Query 預設 True
+    [QUERY_SCENIC]: FILTER_BASIC_TRUE,
+    [QUERY_SCENIC_BY_CITY]: FILTER_BASIC_TRUE,
+    [QUERY_RESTAURANT]: FILTER_BASIC_TRUE,
+    [QUERY_RESTAURANT_BY_CITY]: FILTER_BASIC_TRUE,
+    [QUERY_HOTEL]: FILTER_BASIC_TRUE,
+    [QUERY_HOTEL_BY_CITY]: FILTER_BASIC_TRUE,
+    [QUERY_ACTIVITY]: FILTER_BASIC_TRUE,
+    [QUERY_ACTIVITY_BY_CITY]: '1 == 0', // 活動沒有ZipCode
+}
+Object.freeze(BASIC_FILTER)
+
 const queryFunction = {
     [QUERY_SCENIC]: getScenicSpot,
     [QUERY_SCENIC_BY_CITY]: getScenicSpotByCity, // 發現一般filter也可以用
@@ -145,6 +159,12 @@ export default {
             isShow: false,
         },
         page: 1,
+        currentQueryConfig: {
+            scenic: {},
+            restaurant: {},
+            hotel: {},
+            activity: {},
+        },
         currentQuery: {
             scenic: [],
             restaurant: [],
@@ -159,7 +179,29 @@ export default {
         },
     }),
     getters: {
-        getSkip: state => state.page * baseQueryConfig.top,
+        getSkip: state => (state.page - 1) * baseQueryConfig.top,
+        getQueryTags: state => category => {
+            const config = state.currentQueryConfig[category]
+            const map = new Map()
+            if (!config) return []
+            if (config.keywords) {
+                map.set(config.keywords, true)
+            }
+            if (config.queryList && config.queryList.length > 0) {
+                config.queryList.forEach((item) => {
+                    item.query.forEach(query => { map.set(query, true) })
+                })
+            }
+            return [...map.keys()]
+        },
+        getCurrentQueryPostByCategory: state => category => (state.currentQuery[category] || []),
+        getCurrentQueryPagination: state => ({ category, page }) => {
+            const total = state.currentTotal[category]
+            return {
+                max: total ? Math.ceil(total / baseQueryConfig.top) : 0,
+                now: state.page,
+            }
+        },
     },
     mutations: {
         showPopUp (state) {
@@ -167,6 +209,35 @@ export default {
         },
         hidePopUp (state) {
             state.popUp.isShow = false
+        },
+        saveQueryConfig (state, { config, category }) {
+            console.log('saveQueryConfig', category, config)
+            state.currentQueryConfig[category] = config
+        },
+        resetResultCurrent (state) {
+            state.currentQuery = {
+                scenic: [],
+                restaurant: [],
+                hotel: [],
+                activity: [],
+            }
+        },
+        resetResultTotal (state) {
+            state.currentTotal = {
+                scenic: 0,
+                restaurant: 0,
+                hotel: 0,
+                activity: 0,
+            }
+        },
+        resetQueryConfig (state) {
+            state.page = 1
+            state.currentQueryConfig = {
+                scenic: {},
+                restaurant: {},
+                hotel: {},
+                activity: {},
+            }
         },
         setQueryResult (state, result) {
             const { list, type, length } = result
@@ -178,10 +249,17 @@ export default {
         },
     },
     actions: {
+        resetAll ({ commit }) {
+            commit('resetResultCurrent')
+            commit('resetResultTotal')
+        },
         // ------------ 0: 分類別 (OR) ------------
         query (context, formDate) {
-            console.log('tour/query')
+            console.log('tour/query', formDate)
+            context.commit('resetQueryConfig') // 重置查詢設定
+
             const config = {
+                keywords: formDate.get('keywords'),
                 region: {},
                 countyAll: {},
                 town: {},
@@ -197,7 +275,7 @@ export default {
             // 以物件Folder格式 傳遞
             for (const key of formDate.keys()) {
                 key.split('/').reduce((node, key) => {
-                    if (!node[key]) {
+                    if (!node[key] && key !== 'keywords') {
                         node[key] = {}
                     }
                     return node[key]
@@ -215,7 +293,7 @@ export default {
                     pushActivityQuery(config)
                 }
             })
-            context.dispatch('dispatchQueryStacks', config)
+            return context.dispatch('dispatchQueryStacks', config)
         },
 
         // ------------ 1: 分地區查詢 ------------
@@ -225,73 +303,111 @@ export default {
 
         // ------------ 2: 最後包含字串，送出查詢 (AND) ------------
         dispatchQueryStacks (context, config) {
-            let categoryList = Object.keys(config.category)
-            // 空白則全選
-            categoryList = (categoryList.length > 0)
-                ? categoryList
-                : ['scenic', 'restaurant', 'hotel', 'activity']
-            const requestStacks = []
-            const filter = { // Query 預設 True
-                [QUERY_SCENIC]: '1 == 1',
-                [QUERY_SCENIC_BY_CITY]: '1 == 1',
-                [QUERY_RESTAURANT]: '1 == 1',
-                [QUERY_RESTAURANT_BY_CITY]: '1 == 1',
-                [QUERY_HOTEL]: '1 == 1',
-                [QUERY_HOTEL_BY_CITY]: '1 == 1',
-                [QUERY_ACTIVITY]: '1 == 1',
-                [QUERY_ACTIVITY_BY_CITY]: '1 == 0', // 活動沒有ZipCode
+            context.dispatch('resetAll') // 重置資料
+            return new Promise((resolve, reject) => {
+                let categoryList = Object.keys(config.category)
+                const queryString = config.keywords
+                    ? `(contains(Name,'${config.keywords}') | contains(Description,'${config.keywords}'))`
+                    : FILTER_BASIC_TRUE
+
+                // 若空白 則全選
+                categoryList = (categoryList.length > 0)
+                    ? categoryList
+                    : ['scenic', 'restaurant', 'hotel', 'activity']
+                const requestStacks = []
+                const filter = Object.assign({}, BASIC_FILTER)
+
+                // Merge same category
+                config.queryList.forEach(queryConfig => {
+                    const { category, query } = queryConfig
+
+                    // 非正常操作：不純在查詢的API
+                    if (!queryFunction[category]) return
+
+                    if ( // Filter City
+                        category === QUERY_SCENIC_BY_CITY ||
+                        category === QUERY_RESTAURANT_BY_CITY ||
+                        category === QUERY_HOTEL_BY_CITY ||
+                        category === QUERY_ACTIVITY_BY_CITY
+                    ) {
+                        if (query.length > 0) { // ex. City == '臺北市' | ...
+                            filter[category] = `City == '${query.join('\' | City == \'')}'`
+                        }
+                    } else if ( // Filter ZipCode （活動沒有ZipCode）
+                        category === QUERY_SCENIC ||
+                        category === QUERY_RESTAURANT ||
+                        category === QUERY_HOTEL
+                    ) {
+                        if (query.length > 0) { // ex. ZipCode == '123' | ...
+                            const zipCodeList = query.map(getZipcode)
+                            filter[category] = `ZipCode == '${zipCodeList.join('\' | ZipCode == \'')}'`
+                        }
+                    }
+                })
+
+                // Make request
+                categoryList.forEach((category) => {
+                    let queryFilter = ''
+                    if (category === 'scenic') {
+                        queryFilter = `(${filter[QUERY_SCENIC]}) & (${filter[QUERY_SCENIC_BY_CITY]})`
+                    } else if (category === 'restaurant') {
+                        queryFilter = `(${filter[QUERY_RESTAURANT]}) & (${filter[QUERY_RESTAURANT_BY_CITY]})`
+                    } else if (category === 'hotel') {
+                        queryFilter = `(${filter[QUERY_HOTEL]}) & (${filter[QUERY_HOTEL_BY_CITY]})`
+                    } else if (category === 'activity') { // 活動沒有ZipCode
+                        queryFilter = `(${filter[QUERY_ACTIVITY_BY_CITY]})`
+                    }
+                    console.log({ category, query: `${queryString} & ${queryFilter}` })
+                    const queryConfig = Object.assign({ ...baseQueryConfig }, {
+                        filter: `${queryString} & ${queryFilter}`, // 關鍵字 & 地區
+                        skip: context.getters.getSkip, // 跳過幾筆
+                    })
+
+                    // 送出主要查詢
+                    requestStacks.push(queryFunction[category](queryConfig))
+                    // 儲存查詢
+                    context.commit('saveQueryConfig', { category, config: queryConfig })
+                })
+
+                Promise.allSettled(requestStacks).then(res => {
+                    context.commit('resetResultCurrent')
+                    const ifNoResult = res.every(item => {
+                        console.log(item)
+                        return item.status === 'fulfilled' && item.value.length === 0
+                    })
+                    if (ifNoResult) {
+                        reject(new Error('查無結果'))
+                    } else {
+                        const categoryQuery = []
+                        res.forEach(({ status, value }, index) => {
+                            if (status === 'fulfilled') {
+                                context.commit('setQueryResult', value)
+                            }
+                            if (value.length) {
+                                categoryQuery.push(value.type) // 撈取預設查詢類別
+                            }
+                        })
+                        resolve(categoryQuery[0])
+                    }
+                })
+            })
+        },
+        dispatchPageQuery (context, query) {
+            const { category, page } = query
+            const config = context.state.currentQueryConfig[category]
+            if (context.state.page === page || !config) {
+                return new Promise((resolve, reject) => { resolve(false) })
+            } else {
+                context.state.page = +page // 設定目前頁數 // TODO commit
             }
 
-            // Merge same category // TODO: 關鍵字搜尋
-            config.queryList.forEach(queryConfig => {
-                const { category, query } = queryConfig
-                // 不純在查詢的API
-                if (!queryFunction[category]) return
-
-                if ( // Filter City
-                    category === QUERY_SCENIC_BY_CITY ||
-                    category === QUERY_RESTAURANT_BY_CITY ||
-                    category === QUERY_HOTEL_BY_CITY ||
-                    category === QUERY_ACTIVITY_BY_CITY
-                ) {
-                    if (query.length > 0) {
-                        filter[category] = `City == '${query.join('\' | City == \'')}'`
-                    }
-                } else if ( // Filter ZipCode （活動沒有ZipCode）
-                    category === QUERY_SCENIC ||
-                    category === QUERY_RESTAURANT ||
-                    category === QUERY_HOTEL
-                ) {
-                    if (query.length > 0) {
-                        const zipCodeList = query.map(getZipcode)
-                        filter[category] = `ZipCode == '${zipCodeList.join('\' | ZipCode == \'')}'`
-                    }
-                }
-            })
-
-            categoryList.forEach((category) => {
-                let queryFilter = ''
-                if (category === 'scenic') {
-                    queryFilter = `(${filter[QUERY_SCENIC]}) & (${filter[QUERY_SCENIC_BY_CITY]})`
-                } else if (category === 'restaurant') {
-                    queryFilter = `(${filter[QUERY_RESTAURANT]}) & (${filter[QUERY_RESTAURANT_BY_CITY]})`
-                } else if (category === 'hotel') {
-                    queryFilter = `(${filter[QUERY_HOTEL]}) & (${filter[QUERY_HOTEL_BY_CITY]})`
-                } else if (category === 'activity') { // 活動沒有ZipCode
-                    queryFilter = `(${filter[QUERY_ACTIVITY_BY_CITY]})`
-                }
-                console.log(category, queryFilter)
-                const queryConfig = Object.assign({ ...baseQueryConfig }, {
-                    filter: queryFilter,
-                })
-                requestStacks.push(queryFunction[category](queryConfig))
-            })
-
-            Promise.allSettled(requestStacks).then(res => {
-                res.forEach(({ status, value }, index) => {
-                    if (status === 'fulfilled') {
-                        context.commit('setQueryResult', value)
-                    }
+            config.skip = context.getters.getSkip // 只針對skip設定
+            return new Promise((resolve, reject) => {
+                // 送出相同查詢
+                queryFunction[category](config).then(res => { // 更新查詢結果
+                    context.state.currentQuery[category] = res.list // TODO commit
+                }).catch(err => {
+                    reject(err)
                 })
             })
         },
--- src/views/Tour/Base.vue
+++ src/views/Tour/Base.vue
@@ -1,9 +1,85 @@
 <script setup>
+import { useStore } from 'vuex'
+import { computed, inject, watch } from 'vue'
+
+const store = useStore()
+const isPopUp = computed(() => store.state.tour.popUp.isShow)
+const openPopUp = () => store.commit('tour/showPopUp')
+const hidePopUp = () => store.commit('tour/hidePopUp')
+const scrollInstance = inject('scrollInstance')
+
+const headSearchHandler = (e) => {
+    console.log(e)
+    if (e === 'filter') {
+        openPopUp()
+    } else {
+        // TODO
+    }
+}
 </script>
 <template>
     <div class="tour-base">
-        <TourHeader />
+        <TourHeader
+            :is-search="$route.name !== 'Tour'"
+            @search="headSearchHandler"
+        />
         <slot />
         <TourFooter />
+        <div
+            class="tour-base__pop-up"
+            :class="{
+                '-show': isPopUp,
+                '-hide': !isPopUp,
+            }"
+            @click.self="hidePopUp"
+        >
+            <FormFilter
+                @back="hidePopUp"
+                @submit="hidePopUp"
+            />
+        </div>
     </div>
 </template>
+<style lang="scss">
+$header-height: 84px;
+
+.tour-base {
+    padding-top: $header-height;
+
+    &__pop-up {
+        $duration: 0.4s;
+
+        position: fixed;
+        top: 0;
+        left: 0;
+        padding: 80px;
+        width: 100%;
+        height: 100%;
+        background: rgba(44, 44, 44, 0.8);
+        z-index: map-get($z-index, pop-up);
+        transition: background-color $duration ease-in-out, backdrop-filter $duration/2 ease-in-out;
+        backdrop-filter: blur(30px);
+
+        .form-filter {
+            overflow: auto;
+            margin: 0 auto;
+            width: 800px;
+            max-height: 100%;
+            transition: transform $duration ease-in-out;
+            transform-origin: 50% 120%;
+        }
+
+        &.-hide {
+            background: rgba(44, 44, 44, 0);
+            pointer-events: none;
+            backdrop-filter: blur(0);
+            transition-duration: $duration/2;
+
+            .form-filter {
+                pointer-events: none;
+                transform: scale(0) translateY(100%);
+            }
+        }
+    }
+}
+</style>
