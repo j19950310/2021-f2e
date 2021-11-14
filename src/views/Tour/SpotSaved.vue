2123b9f882c9c4d144e3aa52661f1502dd9fadaf
Jay <jaywu.fe@gmail.com>
2021-11-15 01:39:17 +0800

    [feat] 列表頁css統整

--- src/views/Tour/SpotSaved.vue
+++ src/views/Tour/SpotSaved.vue
@@ -11,7 +11,6 @@ const route = useRoute()
 const title = 'Spot Search' // TODO
 const subtitle = 'Search for a spot by name or by location' // TODO
 const perPage = 12
-const page = ref(1)
 const scrollInstance = inject('scrollInstance')
 const toggleSaveSpot = (spot) => {
     store.commit('tour/toggleSaveSpot', {
@@ -28,6 +27,7 @@ if (!route.query.category) { // 預設category
         },
     })
 }
+const page = ref(route.query.page || 1)
 
 const category = reactive({
     now: 'scenic',
@@ -48,6 +48,7 @@ const createCatButton = (key, name) => (reactive({
     },
 }))
 const tags = ref([])
+const sumOfSpot = computed(() => (store.state.tour.savedQueryIds.length))
 const totalPosts = computed(() => {
     const category = route.query.category || 'scenic'
     return store.state.tour.savedQuery[category]
@@ -75,12 +76,14 @@ watch(route, (to) => {
             class="tour-spot-saved__banner"
         >
             <div class="tour-spot-saved__banner-main">
-                <div class="tour-spot-saved__banner-content">
-                    <div class="tour-spot-saved__banner-content-title">
-                        {{ title }}
-                    </div>
-                    <div class="tour-spot-saved__banner-content-subtitle">
-                        {{ subtitle }}
+                <div class="container">
+                    <div class="tour-spot-saved__banner-content">
+                        <p class="large">
+                            已收藏 <span class="primary">{{ sumOfSpot }}</span> 個地點！
+                        </p>
+                        <p class="medium">
+                            把喜歡的地點保存起來，世世代代流傳下去 ☺️
+                        </p>
                     </div>
                 </div>
             </div>
@@ -150,67 +153,5 @@ watch(route, (to) => {
 </template>
 <style lang="scss">
 $class-name: '.tour-spot-saved';
-#{$class-name} {
-    &__banner {
-        position: relative;
-        overflow: hidden;
-        width: 100%;
-        height: 100%;
-
-        &::before {
-            content: '';
-            display: block;
-            padding-bottom: percentage(580/1440);
-            width: 100%;
-        }
-
-        &__main {
-            position: absolute;
-            top: 0;
-            left: 0;
-            @include size(100%);
-        }
-    }
-
-    &__filter {
-        $space: 24px;
-
-        padding: $space 0;
-        background: color('White');
-        box-shadow: 0 0 24px rgba(0, 0, 0, 0.1);
-
-        &-cat {
-            display: flex;
-            border-bottom: 4px solid #383838;
-            padding: 0;
-            margin-bottom: $space;
-
-            .button-tab-category {
-                flex: 0 0 150px;
-                margin-bottom: -4px;
-            }
-        }
-
-        &-tag {
-            margin: -6px;
-
-            .button-min {
-                margin: 6px;
-            }
-        }
-    }
-
-    &__main {
-        padding-top: 64px;
-        padding-bottom: 64px;
-    }
-
-    &__list {
-        margin: -16px -12px;
-    }
-
-    &__card {
-        padding: 16px 12px;
-    }
-}
+@include spot-list($class-name);
 </style>
